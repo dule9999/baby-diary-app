ee5c3d3f82aa945b0123ef112623c38da05a295a
dule9999 <13601591+dule9999@users.noreply.github.com>
2025-08-29 22:59:03 +0200

    fix formatting

--- frontend/screens/Home/components/BabiesList.tsx
+++ frontend/screens/Home/components/BabiesList.tsx
@@ -4,25 +4,25 @@ import BabyCard from "./BabyCard"
 import { Baby } from "@sharedTypes"
 
 interface BabiesListProps {
-    babies: Baby[],
-    navigateToBabyScreen: (baby: Baby) => void;
+	babies: Baby[],
+	navigateToBabyScreen: (baby: Baby) => void;
 }
 
 const BabiesList: React.FC<BabiesListProps> = ({ babies, navigateToBabyScreen }) => {
-    return(
-        <FlatList
-            data={babies}
-            keyExtractor={(baby) => baby.id}
-            renderItem={({ item }) => (
-                <TouchableOpacity
-                    onPress={() => navigateToBabyScreen(item)}
-                >
-                    <BabyCard baby={item} />
-                </TouchableOpacity>
-                )}
-            style={styles.babiesList}
-        />
-    )
+	return(
+		<FlatList
+				data={babies}
+				keyExtractor={(baby) => baby.id}
+				renderItem={({ item }) => (
+						<TouchableOpacity
+								onPress={() => navigateToBabyScreen(item)}
+						>
+								<BabyCard baby={item} />
+						</TouchableOpacity>
+						)}
+				style={styles.babiesList}
+		/>
+	)
 }
 
 const styles = StyleSheet.create({
